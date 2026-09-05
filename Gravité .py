e1056bd8145c047c3d458d43e2b0e79299330cad
Sophia06Pro <lemsophia864@gmail.com>
2026-09-05 18:44:21 +0200

    étape 5.0 : faire bouger le soleil.

--- Gravité .py
+++ Gravité .py
@@ -22,6 +22,17 @@ G = 4 * math.pi**2  # constante gravitationnelle en UA³ / an² / masse solaire
 M = 1               # masse du Soleil en masse solaire
 dt = 0.01           # temps simulé par image, en années
 
+masses = [
+    1.660e-7,   #Mercure
+    2.447e-6,   #Vénus
+    3.003e-6,   #Terre
+    3.227e-7,   #Mars
+    9.545e-4,   #Jupiter
+    2.857e-4,   #Saturne
+    4.366e-5,   #Uranus
+    5.151e-5    #Neptune
+]  #masses en masses solaires
+
 
 #définir les rayons, les angles initiaux et les vitesses orbitales
 r = [0.39, 0.72, 1.00, 1.52, 5.20, 9.58, 19.2, 30.1]        #en UA dans l'ordre des planètes
@@ -37,10 +48,9 @@ y = [rayon * math.sin(angle) for rayon, angle in zip(r, angles)]
 vx = [-vitesses[i] * math.sin(angles[i]) for i in range(8)]
 vy = [vitesses[i] * math.cos(angles[i]) for i in range(8)]
 
-
  
 
-for etape in range(100):
+for etape in range(10000):
 #déclarer x et y dans une liste chaune vides 
 
 # utiliser scatter de plt afin de dessiner un rond en le nommant le soleil avec label
@@ -50,17 +60,40 @@ for etape in range(100):
 
 
     for i in range(len(planetes)) : #len c'est pour compter le nombre d'une liste
+        
+     # i : planète dont on calcul l’accélération
+     # j : une autre planète qui l’attire
+
+        
+     #accélération de la planète due au soleil
      
      r2 = math.sqrt(x[i]**2 + y[i]**2)
 
-     ax = -G * M * x[i] / r2**3
-     ay = -G * M * y[i] / r2**3     #gravité en x et y dirigé par le soleil
+     ax_total = -G * M * x[i] / r2**3
+     ay_total = -G * M * y[i] / r2**3     #gravité en x et y dirigé par le soleil
      
-     vx[i] = vx[i] + ax * dt
-     vy[i] = vy[i] + ay * dt
+     
+     #accélération de la planètes due aux autres planètes
+     for j in range(len(planetes)) : 
+         
+         if i != j : 
+             
+             dx = x[j] - x[i]
+             dy = y[j] - y[i]
+             
+             distance = math.sqrt(dx**2 + dy**2)
+             
+             ax_total += G * masses[j] * dx / distance**3
+             ay_total += G * masses[j] * dy / distance**3
+         
+     vx[i] = vx[i] + ax_total * dt 
+     vy[i] = vy[i] + ay_total * dt
 
      x[i] = x[i] + vx[i] * dt
      y[i] = y[i] + vy[i] * dt
+     
+     
+     
     
      plt.text(x[i], y[i], planetes[i]) #il nomme et place les planète
 
@@ -69,7 +102,7 @@ for etape in range(100):
     plt.xlim(-32, 32) #limite de l'axe horizontal x
     plt.ylim(-32, 32) #limite de l'axe vertical y
     plt.grid() #ajt la grille au graphique
-    plt.pause(0.1)
+    plt.pause(0.001)
     plt.clf() #efface les anciennes prositions avant de dessiner une nouvelle position
 
 
